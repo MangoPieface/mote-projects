cb82e991b251a7015fbc9ec702354fe86b20ea90
Steve Newstead <steve.newstead@gmail.com>
2018-10-26 19:06:39 +0100

    no idea really, just playing around

--- Halloween.py
+++ Halloween.py
@@ -22,7 +22,29 @@ colors = [
     (0,   255,   0)
 ]    
 
+def knightRider(lightdirection):
+    for channel in range(4):
+        chasingPixels = [-2,-1,0]
+        for pixel in range(mote.get_pixel_count(channel + 1)):
+            r, g, b = colorsToPop[channel]
+            for(i, chasingPixel) in enumerate(chasingPixels):
+                if (chasingPixel >= 0 and chasingPixel <= 16):
+                    mote.set_pixel(channel + lightdirection, chasingPixel, r, g, b)
+                    mote.show()
+                    time.sleep(0.02)
+                chasingPixels[i] = chasingPixel + 1
+            
+                #will this turn off the pixels?
+                if (lightdirection < 0):
+                    lastPixelToRemove = min(chasingPixels)
+                else:
+                    lastPixelToRemove = max(chasingPixels)
 
+                if (lastPixelToRemove > 0 and lastPixelToRemove <= 16):
+                    for pixelToTurnOff in range(lastPixelToRemove - 1):
+                        mote.set_pixel(channel + 1, pixelToTurnOff, 0, 0, 0)
+                        mote.show()
+        colorsToPop.append(colorsToPop.pop(0))
 
 while True:
     colorsToPop = colors    
@@ -37,24 +59,10 @@ while True:
 
     #     colorsToPop.append(colorsToPop.pop(0))
 
-    # Display solid colour test
-    for step in range(4):
-        for channel in range(4):
-            chasingPixels = [-2,-1,0]
-            for pixel in range(mote.get_pixel_count(channel + 1)):
-                r, g, b = colorsToPop[channel]
-                for(i, chasingPixel) in enumerate(chasingPixels):
-                    if (chasingPixel >= 0 and chasingPixel <= 16):
-                        mote.set_pixel(channel + 1, chasingPixel, r, g, b)
-                        mote.show()
-                        time.sleep(0.02)
-                    chasingPixels[i] = chasingPixel + 1
-                
-                    #will this turn off the pixels?
-                    lastPixelToRemove = min(chasingPixels)
-                    if (lastPixelToRemove > 0 and lastPixelToRemove <= 16):
-                        for pixelToTurnOff in range(lastPixelToRemove - 1):
-                            mote.set_pixel(channel + 1, pixelToTurnOff, 0, 0, 0)
-                            mote.show()
+    knightRider(1)
+    knightRider(-1)
 
-        colorsToPop.append(colorsToPop.pop(0))
+    # Display solid colour test
+
+
+        
